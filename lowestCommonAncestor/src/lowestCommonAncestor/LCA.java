fcd72e20799df254fc1fcb427fc7b65df1b46908
aoifesullivan <aoifesullivan@10.6.72.74>
2018-10-01 14:28:08 +0100

    Commit 2

--- lowestCommonAncestor/src/lowestCommonAncestor/LCA.java
+++ lowestCommonAncestor/src/lowestCommonAncestor/LCA.java
@@ -5,6 +5,19 @@ import org.w3c.dom.Node;
 public class LCA <Key extends Comparable<Key>, Value>{
 	Node root; 
 	
+	class Node {
+		private Key key;           				
+		private Value val;         				
+		private Node left, right; 				
+		private int N;             				
+
+		public Node(Key key, Value val, int N) {
+			this.key = key;
+			this.val = val;
+			this.N = N;
+		}
+
+	}
 
 }
 
