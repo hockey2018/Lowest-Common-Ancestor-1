33110502e9b88d4b4cc59e051d069ecece4b67c6
aoifesullivan <aoifesullivan@10.6.72.74>
2018-10-05 15:03:42 +0100

    38

--- lowestCommonAncestor/src/lowestCommonAncestor/LCATest.java
+++ lowestCommonAncestor/src/lowestCommonAncestor/LCATest.java
@@ -2,4 +2,28 @@ package lowestCommonAncestor;
 
 public class LCATest {
 
+public void testLCA() {
+		
+		LCA<Integer, Integer> bst = new LCA<Integer, Integer>();
+		
+		assertSame("Testing LCA for null root", null, bst.lowestCommonAncestor(bst.root, 1, 2));
+		
+		bst.put(7, 7);   
+		bst.put(8, 8);   
+		bst.put(3, 3);   
+		bst.put(1, 1);   
+		bst.put(2, 2);   
+		bst.put(6, 6);   
+		bst.put(4, 4);   
+		bst.put(5, 5);   
+		
+		assertSame("Testing LCA left side", 3, bst.lowestCommonAncestor(bst.root, 2,6));
+		assertSame("Testing LCA right side", 7, bst.lowestCommonAncestor(bst.root, 8,3));
+		assertSame("Testing LCA where LCA is one of the nodes", 7, bst.lowestCommonAncestor(bst.root, 7,8));
+	}
+
+private void assertSame(String string, Object object, Integer lowestCommonAncestor) {
+	// TODO Auto-generated method stub
+	
+}
 }
