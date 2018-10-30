6e5bd997775e13a3ce97be26577319d0edb472d2
aoifesullivan <aoifesullivan@192.168.1.6>
2018-10-30 20:42:35 +0000

    2

--- lowestCommonAncestor/src/DirectedAcyclicForm.java
+++ lowestCommonAncestor/src/DirectedAcyclicForm.java
@@ -1,4 +1,11 @@
 import java.util.Arrays;
 public class DirectedAcyclicForm {
+	private class Node {
+		private Value val;					
+		private Node[] successors;			
 
+		public Node(Value val) {
+			this.val = val;
+		}
+	}
 }
