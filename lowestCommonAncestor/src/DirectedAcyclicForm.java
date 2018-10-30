e7f311169b5c0dec48bb61bfd485f9e8783dd06e
aoifesullivan <aoifesullivan@192.168.1.6>
2018-10-30 20:43:37 +0000

    3rd

--- lowestCommonAncestor/src/DirectedAcyclicForm.java
+++ lowestCommonAncestor/src/DirectedAcyclicForm.java
@@ -8,4 +8,16 @@ public class DirectedAcyclicForm {
 			this.val = val;
 		}
 	}
+	@SuppressWarnings("unchecked")
+	private Node[] nodeList = new DirectedAcyclicForm.Node[0];
+
+	public boolean isEmpty() { 
+		//this is to check if the method is empty 
+		if (size()==0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
