e3993d44bd11aaecd6fb9f77875575f95eca34d1
aoifesullivan <aoifesullivan@10.6.70.31>
2018-11-01 13:52:07 +0000

    commit 2

--- lowestCommonAncestor/src/DirectedAcyclicFormTest.java
+++ lowestCommonAncestor/src/DirectedAcyclicFormTest.java
@@ -1,4 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class DirectedAcyclicFormTest {
 
+	@Test
+	public void testForDirectedGraph(){
+		DirectedAcyclicForm test = new DirectedAcyclicForm(10);
+		test.addEdge(1, 2);
+		test.addEdge(1, 3);
+		test.addEdge(3, 4);
+		test.addEdge(4, 5);
+		test.addEdge(4, 6);
+
+		assertEquals("", 1, test.indegree(4));
+		assertEquals("", 2, test.outdegree(4));
+		assertEquals("", 5, test.E());
+		assertEquals("", 10, test.V());
+		String ans = "[5, 6]";
+		assertEquals("",ans, test.adj(4).toString());
+	}
 }
